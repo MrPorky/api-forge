5ed805f66f8e292c871ea5f635d9daf921773113
❤MrPork❤ <5321067+MrPorky@users.noreply.github.com>
2025-10-20 22:37:46 +0200

    Cleanup

--- packages/mock-dash/src/tests/apiSchema.ts
+++ packages/mock-dash/src/tests/apiSchema.ts
@@ -1,6 +1,19 @@
 import z from 'zod'
 import { defineEndpoint } from '../endpoints'
 
+export const getUsers = defineEndpoint('@get/users', {
+  input: {
+    query: {
+      page: z.string().optional(),
+      limit: z.string().optional(),
+    },
+  },
+  response: z.array(z.object({
+    id: z.string(),
+    name: z.string(),
+  })),
+})
+
 export const getUser = defineEndpoint('@get/users/:id', {
   input: {
     param: {
